438cf8721336944f7958b51d42cc3bbd2f4d515b
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2022-10-18 14:00:54 +0200

    V1

---
+++ PruebaServidor/clase.sql
@@ -0,0 +1,18 @@
+DROP DATABASE clase;
+
+CREATE DATABASE clase;
+
+USE clase;
+
+CREATE TABLE alumno (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    Nombre VARCHAR(50) NOT NULL,
+    Apellido1 VARCHAR(50) NOT NULL,
+    Apellido2 VARCHAR(50) NOT NULL
+);
+
+INSERT INTO alumno (Nombre, Apellido1, Apellido2)
+VALUES 
+    ("Pablo Miguel", "del Castillo", "Barba"),
+    ("Manuel Jesus", "Curtido", "Rosado")
+;
