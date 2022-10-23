63f58808d4359a66f64726f933531e6982ca8624
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2022-10-23 13:06:21 +0200

    V6

--- GraficoTestServidor/graficoTestServer.sql
+++ GraficoTestServidor/graficoTestServer.sql
@@ -11,16 +11,15 @@ CREATE TABLE Opciones_table (
 );
 
 CREATE TABLE Pregunta_table (
-    Id_pregunta INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    Pregunta VARCHAR(50) NOT NULL PRIMARY KEY
+);
+
+CREATE TABLE PreguntaOpciones_table (
+    Id_PreguntaOpciones INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     Pregunta VARCHAR(50) NOT NULL,
-    Id_opcion1 INT UNSIGNED,
-    Id_opcion2 INT UNSIGNED,
-    Id_opcion3 INT UNSIGNED,
-    Id_opcion4 INT UNSIGNED,
-    CONSTRAINT fk_pregunta_opcion1 FOREIGN KEY (Id_opcion1) REFERENCES Opciones_table (Id_opcion),
-    CONSTRAINT fk_pregunta_opcion2 FOREIGN KEY (Id_opcion2) REFERENCES Opciones_table (Id_opcion),
-    CONSTRAINT fk_pregunta_opcion3 FOREIGN KEY (Id_opcion3) REFERENCES Opciones_table (Id_opcion),
-    CONSTRAINT fk_pregunta_opcion4 FOREIGN KEY (Id_opcion4) REFERENCES Opciones_table (Id_opcion)
+    Id_opcion INT UNSIGNED,
+    CONSTRAINT fk_pregunta FOREIGN KEY (Pregunta) REFERENCES Pregunta_table (Pregunta),
+    CONSTRAINT fk_pregunta_opcion FOREIGN KEY (Id_opcion) REFERENCES Opciones_table (Id_opcion)
 );
 
 INSERT INTO Opciones_table (Id_opcion, Texto, Correcto)
@@ -47,11 +46,35 @@ VALUES
     (20, "2", 1)
 ;
 
-INSERT INTO Pregunta_table (Pregunta, Id_opcion1, Id_opcion2, Id_opcion3, Id_opcion4)
+INSERT INTO Pregunta_table (Pregunta)
 VALUES 
-    ("¿De qué color es el caballo blanco de Santiago?", 1, 2, 3, 4),
-    ("¿Como se llama el dueño de Tesla?", 5, 6, 7, 8),
-    ("¿Que es Java?", 9, 10, 11, 12),
-    ("La intensidad de corriente eléctrica se mide utilizando:", 13, 14, 15, 16),
-    ("¿Cuál es el único número que es par y primo?", 17, 18, 19, 20)
+    ("¿De qué color es el caballo blanco de Santiago?"),
+    ("¿Como se llama el dueño de Tesla?"),
+    ("¿Que es Java?"),
+    ("La intensidad de corriente eléctrica se mide utilizando:"),
+    ("¿Cuál es el único número que es par y primo?")
+;
+
+INSERT INTO PreguntaOpciones_table (Pregunta, Id_opcion)
+VALUES 
+    ("¿De qué color es el caballo blanco de Santiago?" , 1),
+    ("¿De qué color es el caballo blanco de Santiago?" , 2),
+    ("¿De qué color es el caballo blanco de Santiago?" , 3),
+    ("¿De qué color es el caballo blanco de Santiago?" , 4),
+    ("¿Como se llama el dueño de Tesla?" , 5),
+    ("¿Como se llama el dueño de Tesla?" , 6),
+    ("¿Como se llama el dueño de Tesla?" , 7),
+    ("¿Como se llama el dueño de Tesla?" , 8),
+    ("¿Que es Java?" , 9),
+    ("¿Que es Java?" , 10),
+    ("¿Que es Java?" , 11),
+    ("¿Que es Java?" , 12),
+    ("La intensidad de corriente eléctrica se mide utilizando:" , 13),
+    ("La intensidad de corriente eléctrica se mide utilizando:" , 14),
+    ("La intensidad de corriente eléctrica se mide utilizando:" , 15),
+    ("La intensidad de corriente eléctrica se mide utilizando:" , 16),
+    ("¿Cuál es el único número que es par y primo?" , 17),
+    ("¿Cuál es el único número que es par y primo?" , 18),
+    ("¿Cuál es el único número que es par y primo?" , 19),
+    ("¿Cuál es el único número que es par y primo?" , 20)
 ;
