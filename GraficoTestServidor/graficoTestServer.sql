cd355ee702def1e4ee10427344fe2b1ecee6082e
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2022-10-24 13:50:46 +0200

    V8

--- GraficoTestServidor/graficoTestServer.sql
+++ GraficoTestServidor/graficoTestServer.sql
@@ -11,14 +11,15 @@ CREATE TABLE Opciones_table (
 );
 
 CREATE TABLE Pregunta_table (
-    Pregunta VARCHAR(100) NOT NULL PRIMARY KEY
+    Id_pregunta INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    Pregunta VARCHAR(100) UNIQUE NOT NULL
 );
 
 CREATE TABLE PreguntaOpciones_table (
     Id_PreguntaOpciones INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    Pregunta VARCHAR(100) NOT NULL,
+    Id_pregunta INT UNSIGNED,
     Id_opcion INT UNSIGNED,
-    CONSTRAINT fk_pregunta FOREIGN KEY (Pregunta) REFERENCES Pregunta_table (Pregunta),
+    CONSTRAINT fk_pregunta FOREIGN KEY (Id_pregunta) REFERENCES Pregunta_table (Id_pregunta),
     CONSTRAINT fk_pregunta_opcion FOREIGN KEY (Id_opcion) REFERENCES Opciones_table (Id_opcion)
 );
 
@@ -55,26 +56,26 @@ VALUES
     ("¿Cuál es el único número que es par y primo?")
 ;
 
-INSERT INTO PreguntaOpciones_table (Pregunta, Id_opcion)
+INSERT INTO PreguntaOpciones_table (Id_pregunta, Id_opcion)
 VALUES 
-    ("¿De qué color es el caballo blanco de Santiago?" , 1),
-    ("¿De qué color es el caballo blanco de Santiago?" , 2),
-    ("¿De qué color es el caballo blanco de Santiago?" , 3),
-    ("¿De qué color es el caballo blanco de Santiago?" , 4),
-    ("¿Como se llama el dueño de Tesla?" , 5),
-    ("¿Como se llama el dueño de Tesla?" , 6),
-    ("¿Como se llama el dueño de Tesla?" , 7),
-    ("¿Como se llama el dueño de Tesla?" , 8),
-    ("¿Que es Java?" , 9),
-    ("¿Que es Java?" , 10),
-    ("¿Que es Java?" , 11),
-    ("¿Que es Java?" , 12),
-    ("La intensidad de corriente eléctrica se mide utilizando:" , 13),
-    ("La intensidad de corriente eléctrica se mide utilizando:" , 14),
-    ("La intensidad de corriente eléctrica se mide utilizando:" , 15),
-    ("La intensidad de corriente eléctrica se mide utilizando:" , 16),
-    ("¿Cuál es el único número que es par y primo?" , 17),
-    ("¿Cuál es el único número que es par y primo?" , 18),
-    ("¿Cuál es el único número que es par y primo?" , 19),
-    ("¿Cuál es el único número que es par y primo?" , 20)
+    (1 , 1),
+    (1 , 2),
+    (1 , 3),
+    (1 , 4),
+    (2 , 5),
+    (2 , 6),
+    (2 , 7),
+    (2 , 8),
+    (3 , 9),
+    (3 , 10),
+    (3 , 11),
+    (3 , 12),
+    (4 , 13),
+    (4 , 14),
+    (4 , 15),
+    (4 , 16),
+    (5 , 17),
+    (5 , 18),
+    (5 , 19),
+    (5 , 20)
 ;
